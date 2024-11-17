2d4329768f776e7e0e3d9c42625a35b97cee1194
bariahEbrahim <bareah.0.2@gmail.com>
2024-11-18 00:28:45 +0300

    Lab 5 and 6

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path
+from django.urls import path # type: ignore
 from . import views  # Import the views from the current directory
 
 urlpatterns = [
     path('', views.index),  # Map the root URL of bookmodule to the 'index' view
     path('index2/<int:val1>/', views.index2),  # Map '/index2/' to 'index2' view
-    path('<int:bookId>', views.viewbook)
+    path('<int:bookId>', views.viewbook),
+    path('html5/links/', views.html5_links),  # New path for Task 1
+    path('html5/text/formatting/', views.text_formatting),  # New URL for Task 2
+    path('html5/listing/', views.html5_listing),  # New URL for Task 3
+    path('html5/table/', views.html5_table),  # New URL for Task 4
 
+    path('', views.index, name= "books.index"),
+    path('list_books/', views.list_books, name= "books.list_books"),
+    path('one_book/', views.one_book, name="books.one_book"),  # Static path for one_book
+    path('aboutus/', views.aboutus, name="books.aboutus"),
+    path('search', views.search_books, name='search_books'), #lap6  
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse # type: ignore
 
+# def index(request):
+#     name = request.GET.get("name") or "world!" #add this line
+#     return render(request, "bookmodule/index.html" , {"name": name}) #your render line
 def index(request):
-    name = request.GET.get("name") or "world!" #add this line
-    return render(request, "bookmodule/index.html" , {"name": name}) #your render line
+ return render(request, "bookmodule/index.html")
+def list_books(request):
+ books = [
+        {'id': 1, 'title': 'Internet & World Wide Web How to Program'},
+        {'id': 2, 'title': 'C++ How to Program, Late Objects Version'},
+        {'id': 3, 'title': 'Images in Another Folder'},
+    ]
+ return render(request, 'bookmodule/list_books.html', {'books': books})
+#  return render(request, 'bookmodule/list_books.html')
+def viewbook(request, bookId):
+ return render(request, 'bookmodule/one_book.html')
+def aboutus(request):
+ return render(request, 'bookmodule/aboutus.html')
+
+def one_book(request):
+    return render(request, "bookmodule/one_book.html")
 
 
 def index2(request, val1 = 0): #add the view function (index2)
@@ -19,4 +36,51 @@ def viewbook(request, bookId):
     if book2['id'] == bookId: targetBook = book2
     context = {'book':targetBook} # book is the variable name accessible by the template
     return render(request, 'bookmodule/show.html', context)
+
+
+def html5_links(request):
+    return render(request, 'bookmodule/links.html')
+
+def text_formatting(request):
+    return render(request, 'bookmodule/formatting.html')
+
+def html5_listing(request):
+    return render(request, 'bookmodule/listing.html')
+
+
+def html5_table(request):
+    return render(request, 'bookmodule/table.html')
+
+
+def search_books(request):
+    books = []
+    if request.method == "POST":
+        keyword = request.POST.get('keyword', '').lower()
+        is_title = request.POST.get('option1')
+        is_author = request.POST.get('option2')
+
+        # Retrieve the book list
+        all_books = __getBooksList()
+
+        # Filter books based on the form input
+        for book in all_books:
+            contains = False
+            if is_title and keyword in book['title'].lower():
+                contains = True
+            if is_author and keyword in book['author'].lower():
+                contains = True
+
+            if contains:
+                books.append(book)
+
+    return render(request, 'bookmodule/search.html', {'books': books})
+
+
+def __getBooksList():
+    return [
+        {'id': 12344321, 'title': 'Continuous Delivery', 'author': 'J. Humble and D. Farley'},
+        {'id': 56788765, 'title': 'Reversing: Secrets of Reverse Engineering', 'author': 'E. Eilam'},
+        {'id': 43211234, 'title': 'The Hundred-Page Machine Learning Book', 'author': 'Andriy Burkov'},
+    ]
+
 
